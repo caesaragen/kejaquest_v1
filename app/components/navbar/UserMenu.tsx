9893af202c5a9da7c73c949e67cfe3c876657481
Joel Aduma <caesaragen@gmail.com>
2023-06-29 16:52:48 +0300

    feat(landing-page):add prettier linting to project root

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,53 +1,47 @@
-"use client";
-import { useState, useCallback } from 'react'
-import Avatar from "../Avatar";
-import { AiOutlineMenu } from "react-icons/ai";
+'use client';
+import { useState, useCallback } from 'react';
+import Avatar from '../Avatar';
+import { AiOutlineMenu } from 'react-icons/ai';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
-    const registerModal = useRegisterModal();
-    const [isOpen, setIsOpen] = useState(false)
-    const toggleOpen = useCallback(() => {
-        setIsOpen((value) => !value)
-    }, [])
-    return (
-        <div className="relative">
-            <div className="flex flex-row items-center gap-3">
-                <div
-                    // onClick={()=> {console.log("cl")}}
-                    className="hidden md:block text-sm font-semibold py-3 rounded-full px-4 hover:bg-neutral-100 transition cursor-pointer text-rose-500"
-                >
-                    KejaQuest Find your Home
-                </div>
-                <div
-                    onClick={toggleOpen}
-                    className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-                >
-                    <AiOutlineMenu />
-                    <div className="hidden md:block">
-                        <Avatar />
-                    </div>
-                </div>
-            </div>
-            {isOpen && (
-                <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
-                    <div className="flex flex-col cursor-pointer">
-                        <>
-                            <MenuItem
-                                onClick={() => { }}
-                                label='login'
-                            />
-                            <MenuItem
-                                onClick={registerModal.onOpen}
-                                label='signup'
-                            />
-                        </>
-                    </div>
-                </div>
-            )}
+  const registerModal = useRegisterModal();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          // onClick={()=> {console.log("cl")}}
+          className="hidden cursor-pointer rounded-full px-4 py-3 text-sm font-semibold text-rose-500 transition hover:bg-neutral-100 md:block"
+        >
+          KejaQuest Find your Home
         </div>
-    );
+        <div
+          onClick={toggleOpen}
+          className="flex cursor-pointer flex-row items-center gap-3 rounded-full border-[1px] border-neutral-200 p-4 transition hover:shadow-md md:px-2 md:py-1"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="absolute right-0 top-12 w-[40vw] overflow-hidden rounded-xl bg-white text-sm shadow-md md:w-3/4">
+          <div className="flex cursor-pointer flex-col">
+            <>
+              <MenuItem onClick={() => {}} label="login" />
+              <MenuItem onClick={registerModal.onOpen} label="signup" />
+            </>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserMenu;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,36 @@
-import './globals.css'
-import { Nunito } from 'next/font/google'
-import Navbar from './components/navbar/Navbar'
-import ClientOnly from './components/ClientOnly'
-import RegisterModal from './components/modals/RegisterModal'
-import ToastProvider from './providers/ToastProvider'
+import './globals.css';
+import { Nunito } from 'next/font/google';
+import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from './components/modals/RegisterModal';
+import ToastProvider from './providers/ToastProvider';
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'KejaQuest',
   description: 'KejaQuest',
-}
+};
 
 const font = Nunito({
-  subsets: ["latin"]
-})
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <ToastProvider/>
-        <RegisterModal/>
-        <Navbar/>
+          <ToastProvider />
+          <RegisterModal />
+          <Navbar />
         </ClientOnly>
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
